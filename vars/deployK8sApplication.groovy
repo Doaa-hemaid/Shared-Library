4748ba03e50bf877ba2701515defb75ef1af0194
Doaa Hemaid <doaahemaid01@gmail.com>
2024-12-14 22:31:06 +0200

    Update deployK8sApplication.groovy

--- vars/deployK8sApplication.groovy
+++ vars/deployK8sApplication.groovy
@@ -1,11 +1,11 @@
 def call(String namespace, String dockerImage, String k8sCredentials, String minikubeIp) {
     withCredentials([string(credentialsId: k8sCredentials, variable: 'api_token')]) {
         // Define the common kubectl command prefix
-        def kubectlCommand = "kubectl --token $api_token --server https://${minikubeIp}:8443 --insecure-skip-tls-verify=true --validate=false"
+        def kubectlCommand = "kubectl --token $api_token --server https://${minikubeIp}:8443 --insecure-skip-tls-verify=true"
 
         sh """
             ${kubectlCommand} apply -f myapp.yaml
-            ${kubectlCommand} get deployments -n ${namespace}
+            kubectl get deployments -n ${namespace}
             ${kubectlCommand} get services -n ${namespace}
             
         """
